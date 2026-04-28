2b7b44e539cc50e351380b8e1957d299bd05fae0
bgrasbgras1982 <bgrasbgras1982@gmail.com>
2026-04-28 08:21:38 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
+    //ecrire un programme C++ qui affiche le nombre de presence de chaque chiffre dans un entier fourni par l'utilisateur
     cout << "bonjour Oumou";
     return 0;
 }
