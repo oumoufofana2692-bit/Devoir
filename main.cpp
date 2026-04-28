b8b162187c82a0a2dcc5b427004753f73fe615bf
oumoufofana2692 <oumoufofana2692@gmail.com>
2026-04-28 14:35:26 +0000

    RESOLUTION DU DEVOIR

--- main.cpp
+++ main.cpp
@@ -2,6 +2,31 @@
 using namespace std;
 int main()
 {
-    cout << "bonjour Oumou, c'est moi la star \n";
+    long long n;
+    cout <<" Entrez un entier :";
+    cin >>n;
+//Tableau pour compter les chiffres de 0 a 9
+int compteur[10] = {0};
+//Cas ou le nombre est negatif
+if (n<0){
+    n = -n;
+}
+//cas special si n = 0
+if (n == 0) {
+    compteur[0]++;
+}
+//Parcours du nombre
+while (n>0) {
+    int chiffre = n % 10;
+    compteur[chiffre]++;
+    n =n / 10;
+}
+//Affichage des resultats
+cout <<"occurences des chiffres:" <<endl;
+for (int i = 0; i < 10; i++) {
+    if (compteur[i] > 0){
+        cout << " chiffre " <<i<< " : " << compteur[i] << endl;
+    }
+}
  return 0;
 }
