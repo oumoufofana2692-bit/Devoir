546e6e6de5d7fbe936a530421bdc3fafff8bf651
oumoufofana2692 <oumoufofana2692@gmail.com>
2026-05-12 14:33:30 +0000

    ajout d'autres fonctionnalites

--- main.cpp
+++ main.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
+#include <memory>
 using namespace std;
 int main()
 {
-    long long n;
+    auto n= make_unique<long long >();
+    auto compteur= make_unique<int[]>(10);
+    int chiffre;
     cout <<" Entrez un entier :";
-    cin >>n;
-//Tableau pour compter les chiffres de 0 a 9
-int compteur[10] = {0};
-//Cas ou le nombre est negatif
-if (n<0){
-    n = -n;
+    cin >>*n;
+if (*n<0){
+    *n = -*n;
 }
-//cas special si n = 0
-if (n == 0) {
+if (*n == 0) {
     compteur[0]++;
 }
-//Parcours du nombre
-while (n>0) {
-    int chiffre = n % 10;
+while (*n>0) {
+    chiffre = *n % 10;
     compteur[chiffre]++;
-    n =n / 10;
+    *n /= 10;
 }
-//Affichage des resultats
 cout <<"occurences des chiffres:" <<endl;
-for (int i = 0; i < 10; i++) {
+for (int i=0; i < 10; i++) {
     if (compteur[i] > 0){
         cout << " chiffre " <<i<< " : " << compteur[i] << endl;
     }
